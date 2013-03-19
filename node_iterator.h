1655e042c3f67761829f5c788a4c9334cf0e6e91
Joshua Underwood <joshuaunderwood7@gmail.com>
2013-03-19 00:02:05 -0600

    Bug fixes to node_iterator.h: Syntax corrections

--- node_iterator.h
+++ node_iterator.h
@@ -1,27 +1,25 @@
-ifndef NODE_ITERATOR
-define NODE_ITERATOR
-
-class iterator
-        iterator(const iterator&);
-        ~iterator();
-        iterator& operator=(const iterator&);
-        iterator& operator++();
-        reference operator*() const;
-        friend void swap(const iterator& lhs, const iterator& rhs); 
-};
-
+#ifndef NODE_ITERATOR
+#define NODE_ITERATOR
 
 template <typename T>
-class node_iterator : iterator
+class node_iterator 
 {
-        iterator operator++(int);
-        value_type operator*() const;
-        pointer operator->() const;
-        friend bool operator==(const iterator&, const iterator&);
-        friend bool operator!=(const iterator&, const iterator&); 
+    public:
+        node_iterator(const node_iterator<T>& input);
+        ~node_iterator();
+        node_iterator<T>& operator=(const node_iterator<T>& input);
+        node_iterator<T>& operator++();
+        node_iterator<T>& operator*() const;
+        node_iterator<T>* operator->() const;
+//        friend void swap(node_iterator<T>& lhs, node_iterator<T>& rhs); 
+//        friend bool operator==(const node_iterator<T>& lhs, const node_iterator<T>& rhs);
+//        friend bool operator!=(const node_iterator<T>& lhs, const node_iterator<T>& rhs); 
+    private:
+        T data;
+        list<T>::node* link;
 };
 
 
-#include node_iterator.tem
+#include "node_iterator.tem"
 
-endif
+#endif
